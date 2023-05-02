b5d59cd7a2847a8026056a93a54c694d2f45917d
khizooo <khizar.imtiaz@vaival.com>
2023-05-02 10:29:09 +0500

    A

---
+++ Main/CODE/WEB/studorlio-master/External/Arts.js
@@ -0,0 +1,68 @@
+window.MyArts = `{
+				    "MyArts": {
+				        "Title": "My Arts",
+				 	"Types": [
+				            {
+						"ID": 1,
+				                "Title": "Knife Paintings",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                       "LI": "https://placeimg.com/640/480/arch",
+				                "SD": "Art SD",
+				                "LD": "Art LD"
+				            },
+				            {
+						"ID": 2,
+				                "Title": "Sticky Notes Drawing",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                      "LI": "https://placeimg.com/640/480/arch",
+				                "SD": "Art SD",
+				                "LD": "Art LD"
+				            }
+					],
+				        "Data": [
+				            {
+						"ID": 1,
+				                "Title": "Big Bang Theory",
+				                "SI": "Arts/1/Big-Bang-Theory-Image.avif",
+				                "MI": "Arts/1/Big-Bang-Theory-Image.avif",
+		   	                      "LI": "Arts/1/Big-Bang-Theory-Image.avif",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "Arts/1/Big-Bang-Theory.html"
+				            },
+				{
+						"ID": 2,
+				                "Title": "Labor Day",
+				                "SI": "Arts/2/Labor-Day.png",
+				                "MI": "Arts/2/Labor-Day.png",
+		   	                      "LI": "Arts/2/Labor-Day.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "Arts/2/Labor-Day.html"
+				            },
+				{
+						"ID": 3,
+   "Title": "Upcomings",
+				                "SI": "assets/img/Upcoming.png",
+				                "MI": "assets/img/Upcoming.png",
+		   	                      "LI": "assets/img/Upcoming.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "http://khizooo.com/Arts/1/Big-Bang-Theory.html"
+				            },
+				{
+						"ID": 4,
+				                "Title": "Upcomings",
+
+				                "SI": "assets/img/Upcoming.png",
+				                "MI": "assets/img/Upcoming.png",
+		   	                      "LI": "assets/img/Upcoming.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "http://khizooo.com/Arts/1/Big-Bang-Theory.html"
+				            }
+				        ]
+				    }
+				}`;
---
+++ Main/CODE/WEB/studorlio-master/External/Tech.js
@@ -0,0 +1,67 @@
+window.MyTech = `{
+				    "MyTech": {
+				        "Title": "My Tech",
+				 	"Types": [
+				            {
+						"ID": 1,
+				                "Title": "BlockChain",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                        "LI": "https://placeimg.com/640/480/arch",
+				                "SD": "BlockChain",
+				                "LD": "BlockChain"
+				            },
+				            {
+						"ID": 2,
+				                "Title": ".NET Technologies",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                        "LI": "https://placeimg.com/640/480/arch",
+				                "SD": ".NET Technologies",
+				                "LD": ".NET Technologies"
+				            }
+					],
+				        "Data": [
+				            {
+						"ID": 1,
+				                "Title": "What is BlockChain?",
+				                "SI": "Tech/1/What-is-BlockChain.png",
+				                "MI": "Tech/1/What-is-BlockChain.png",
+		   	                        "LI": "Tech/1/What-is-BlockChain.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "Tech/1/What-Is-BlockChain.html"
+				            },
+				{
+						"ID": 2,
+				                "Title": "Blockchain - Decentralization",
+				                "SI": "Tech/2/BlockChain-Decentralization.png",
+				                "MI": "Tech/2/BlockChain-Decentralization.png",
+		   	                        "LI": "Tech/2/BlockChain-Decentralization.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "Tech/2/BlockChain-Decentralization.html"
+				            },
+					   {
+						"ID": 3,
+   						"Title": "Upcomings",
+				                "SI": "assets/img/Upcoming.png",
+				                "MI": "assets/img/Upcoming.png",
+		   	                        "LI": "assets/img/Upcoming.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "Tech/1/What-Is-BlockChain.html"
+				            },
+					   {
+						"ID": 4,
+				                "Title": "Upcomings",
+				                "SI": "assets/img/Upcoming.png",
+				                "MI": "assets/img/Upcoming.png",
+		   	                        "LI": "assets/img/Upcoming.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "Tech/1/What-Is-BlockChain.html"
+				            }
+				        ]
+				    }
+				}`;
