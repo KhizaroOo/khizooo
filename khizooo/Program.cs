bb1ae76273650f7c2286a6d1a3f3fd01f34012bc
khizooo <khizar.imtiaz@vaival.com>
2024-06-04 04:51:06 +0500

    A

--- khizooo/Program.cs
+++ khizooo/Program.cs
@@ -7,5 +7,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+await app.RunAsync();
+
+
+
+//await builder.Build().RunAsync();
 
