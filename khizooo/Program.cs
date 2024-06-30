f242a7e48091efc7016c7680d7d3e2fc5b91cb00
khizooo <khizar.imtiaz@vaival.com>
2024-06-30 11:09:46 +0500

    A

--- khizooo/Program.cs
+++ khizooo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using khizooo;
+using Microsoft.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
