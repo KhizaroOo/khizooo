ee1afb76d1352fea59e4aa692c71602c3b4256ad
khizooo <khizar.imtiaz@vaival.com>
2024-07-27 03:11:45 +0500

    A

---
+++ khizooo-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace khizooo_web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
