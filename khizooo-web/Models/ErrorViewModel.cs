d7669ffbed8d5d199edfdcbb7f2ed069c8777bfe
khizooo <khizar.imtiaz@vaival.com>
2024-07-31 02:08:43 +0500

    Remove Extra Project

--- khizooo-web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace khizooo_web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
---
+++ khizooo/wwwroot/artworks/js/artworks.js
@@ -0,0 +1,10 @@
+﻿
+function Polazized() {
+    var polaroids = document.querySelectorAll('.polaroid');
+    polaroids.forEach(item => {
+        const randomRotation = Math.floor(Math.random() * (6 - -6 + 1) + -6);
+        item.style.transform = `rotate(${randomRotation}deg)`
+    });
+}
+
+Polazized();
