3a44774f22bd7dc6da9e2b95abe1c333c6ce96f8
Khizar Imtiaz <khizar.imtiaz@gmail.com>
2025-08-19 16:29:55 +0500

    A1

---
+++ khizooo/AppData/Calculator.cs
@@ -0,0 +1,57 @@
+﻿
+namespace khizooo.AppData
+{
+
+    public class Calculator
+    {
+        public string Slug { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Count { get; set; }
+    }
+
+    public class MyCalculators
+    {
+
+        private List<Calculator> MyAllCalculators = new List<Calculator>()
+        {
+            new Calculator() { Slug = "finance-calculator", Title = "Finance Calculator", Description = "Calculate finance related calculations with ease." },
+            new Calculator() { Slug = "math-calculator", Title = "Math Calculator", Description = "Calculate math related calculations with ease." },
+            new Calculator() { Slug = "everyday-life-calculator", Title = "Everyday Life Calculator", Description = "Calculate everyday life related calculations with ease." },
+            new Calculator() { Slug = "biology-calculator", Title = "Biology Calculator", Description = "Calculate biology related calculations with ease." },
+            new Calculator() { Slug = "chemistry-calculator", Title = "Chemistry Calculator", Description = "Calculate chemistry related calculations with ease." },
+            new Calculator() { Slug = "physics-calculator", Title = "Physics Calculator", Description = "Calculate physics related calculations with ease." },
+            new Calculator() { Slug = "color-tools", Title = "Color Tools", Description = "From color palettes to color shades, we have you covered." },
+            new Calculator() { Slug = "health-calculator", Title = "Health Calculator", Description = "Calculate health related calculations with ease." },
+            new Calculator() { Slug = "construction-calculator", Title = "Construction Calculator", Description = "Calculate construction related calculations with ease." },
+            new Calculator() { Slug = "unit-conversion-calculator", Title = "Unit Conversion Calculator", Description = "Calculate conversion related calculations with ease." },
+            new Calculator() { Slug = "food-calculator", Title = "Food Calculator", Description = "Calculate food related calculations with ease." },
+            new Calculator() { Slug = "sports-calculator", Title = "Sports Calculator", Description = "Calculate sports related calculations with ease." },
+            new Calculator() { Slug = "text-tools", Title = "Text Tools", Description = "Text analysis tools for word counting, unscrambling, and manipulation." },
+            new Calculator() { Slug = "fun-calculator", Title = "Fun Calculator", Description = "Fun calculator for all your computation needs." },
+            new Calculator() { Slug = "time-date", Title = "Time & Date", Description = "Calculate time and date related calculations with ease." },
+            new Calculator() { Slug = "photography", Title = "Photography", Description = "Photography related calculations with ease." },
+            new Calculator() { Slug = "crafting", Title = "Crafting", Description = "Crafting related calculations with ease." },
+            new Calculator() { Slug = "automotive-calculator", Title = "Automotive Calculator", Description = "Automotive related calculations with ease." },
+            new Calculator() { Slug = "travel-calculator", Title = "Travel Calculator", Description = "Travel related calculations with ease." }
+        };
+
+
+        public List<Calculator> GetMyCalculators(int Count)
+        {
+            List<Calculator> Data = new List<Calculator>();
+            Data = MyAllCalculators.Take(Count).ToList();
+            return Data;
+        }
+
+        public Calculator GetMyCalculator(string Slug)
+        {
+            Calculator Data = new Calculator();
+            Data = MyAllCalculators.FirstOrDefault(A => A.Slug == Slug);
+            return Data;
+        }
+
+    }
+
+}
---
+++ khizooo/AppData/Converter.cs
@@ -0,0 +1,49 @@
+﻿
+namespace khizooo.AppData
+{
+
+    public class Converter
+    {
+        public string Slug { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Count { get; set; }
+    }
+
+    public class MyConverters
+    {
+
+        private List<Converter> MyAllConverters = new List<Converter>()
+        {
+            new Converter() { Slug = "angle-converter", Title = "Angle Converter", Description = "Convert between degrees, radians, gradians, and more." },
+            new Converter() { Slug = "area-converter", Title = "Area Converter", Description = "Convert between square meters, acres, hectares, and more." },
+            new Converter() { Slug = "data-storage-converter", Title = "Data Storage Converter", Description = "Convert between bytes, kilobytes, megabytes, gigabytes, etc." },
+            new Converter() { Slug = "energy-converter", Title = "Energy Converter", Description = "Convert between joules, watt-hours, BTU, and more." },
+            new Converter() { Slug = "force-converter", Title = "Force Converter", Description = "Convert between newtons, pound-force, kilogram-force, and more." },
+            new Converter() { Slug = "length-converter", Title = "Length Converter", Description = "Convert between meters, feet, inches, kilometers, miles, and more." },
+            new Converter() { Slug = "weight-converter", Title = "Weight Converter", Description = "Convert between kilograms, pounds, ounces, grams, and more." },
+            new Converter() { Slug = "power-converter", Title = "Power Converter", Description = "Convert between watts, kilowatts, horsepower, etc." },
+            new Converter() { Slug = "pressure-converter", Title = "Pressure Converter", Description = "Convert between pascals, bars, atmospheres, PSI, and more." },
+            new Converter() { Slug = "temperature-converter", Title = "Temperature Converter", Description = "Convert between Celsius, Fahrenheit, Kelvin, etc." },
+            new Converter() { Slug = "time-converter", Title = "Time Converter", Description = "Convert between seconds, minutes, hours, days, and more." },
+            new Converter() { Slug = "volume-converter", Title = "Volume Converter", Description = "Convert between liters, gallons, cubic meters, fluid ounces, etc." }
+        };
+
+        public List<Converter> GetMyConverters(int Count)
+        {
+            List<Converter> Data = new List<Converter>();
+            Data = MyAllConverters.Take(Count).ToList();
+            return Data;
+        }
+
+        public Converter GetMyConverter(string Slug)
+        {
+            Converter Data = new Converter();
+            Data = MyAllConverters.FirstOrDefault(A => A.Slug == Slug);
+            return Data;
+        }
+
+    }
+
+}
